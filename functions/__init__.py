b97238101392b3b0b9d3f4b39545fe20e53143c2
guilherme.reis6 <guilherme.reis@paggo.ai>
2026-03-05 23:15:23 -0300

    adds network topology plotter

--- functions/__init__.py
+++ functions/__init__.py
@@ -2,9 +2,11 @@
 
 Public API:
     from functions.live_plot import RealTimeStepPlotter
+    from functions.telecom_network_plot import TelecomNetworkPlotter
 """
 
 from .live_plot import RealTimeStepPlotter  # noqa: F401
+from .telecom_network_plot import TelecomNetworkPlotter  # noqa: F401
 from .logging import (  # noqa: F401
     RunDirectoryManager,
     save_config,
@@ -17,6 +19,7 @@ from .logging import (  # noqa: F401
 
 __all__ = [
     "RealTimeStepPlotter",
+    "TelecomNetworkPlotter",
     "RunDirectoryManager",
     "save_config",
     "save_environment_snapshot",
